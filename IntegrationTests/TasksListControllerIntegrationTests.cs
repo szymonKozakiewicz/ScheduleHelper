8d744ff592937cfad5f8f48057444d97721518e1
Szymon Kozakiewicz <sz.kozakiewicz@gmail.com>
2023-12-28 16:27:22 +0100

    Add TasksListController and integration test for TasksList route

---
+++ IntegrationTests/TasksListControllerIntegrationTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using ScheduleHelper.UI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ScheduleHelper.IntegrationTests
+{
+    public class TasksListControllerIntegrationTests:IClassFixture<WebApplicationFactory<Program>>
+    {
+        HttpClient client;
+        public TasksListControllerIntegrationTests(WebApplicationFactory<Program> factory)
+        {
+            client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetTasksListView_ShouldReturnView()
+        {
+            HttpResponseMessage response = await client.GetAsync("/");
+            response.Should().BeSuccessful();
+
+            
+        }
+    }
+}
